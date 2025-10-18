359489780f8a106970091805ed09ffd32d3a3498
benjipull <benjamin.pullicino@gmail.com>
2025-10-18 09:00:50 +0200

    added ga4

--- frontend-react/src/main.tsx
+++ frontend-react/src/main.tsx
@@ -1,11 +1,21 @@
-import { createRoot } from 'react-dom/client'
-import { ThemeProvider } from 'next-themes'
-import App from './App.tsx'
-import './index.css'
-import './App.css'
+import React from "react";
+import { createRoot } from "react-dom/client";
+import { ThemeProvider } from "next-themes";
+import App from "./App.tsx";
+import "./index.css";
+import "./App.css";
+import ReactGA from "react-ga4";
+
+// ✅ Initialize Google Analytics (replace with your real Measurement ID)
+ReactGA.initialize("G-Q9EHZF08FX");
+
+// Send first pageview when app loads
+ReactGA.send({ hitType: "pageview", page: window.location.pathname + window.location.search });
 
 createRoot(document.getElementById("root")!).render(
-  <ThemeProvider attribute="class" defaultTheme="light" enableSystem>
-    <App />
-  </ThemeProvider>
+  <React.StrictMode>
+    <ThemeProvider attribute="class" defaultTheme="light" enableSystem>
+      <App />
+    </ThemeProvider>
+  </React.StrictMode>
 );
